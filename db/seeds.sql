1a75249c62e42f36c5bc3b41c1d0163ffd82df14
Raj Patel <RajPatel@lawn-128-61-0-234.lawn.gatech.edu>
2018-05-19 08:59:12 -0400

    w/o limit of 3 burgers

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('In-N-Out Burger');
+INSERT INTO burger (name) VALUES ('Burger King');
+INSERT INTO burger (name) VALUES ('Burger 3');
+INSERT INTO burger (name) VALUES ('Burger 4');
+INSERT INTO burger (name) VALUES ('Burger 5');
+INSERT INTO burger (name) VALUES ('Burger 6');
