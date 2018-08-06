179d66259abbe22b54e6f1054c0c7e8663bb4a90
Raj Patel <RajPatel@Rajs-MacBook.local>
2018-08-06 12:41:02 -0400

    tested

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,16 +9,16 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
-      burger: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/burger", function(req, res) {
+router.post("/burgers", function(req, res) {
   burger.create([
-    "name"
+    "burger_name"
   ], [
     req.body.burger_name
   ], function(result) {
@@ -27,22 +27,19 @@ router.post("/api/burger", function(req, res) {
   });
 });
 
-router.post("/api/burger/:id", function(req, res) {
+router.post("/burgers/update/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
-    devoured:true
+    devoured: true
   }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.redirect('/');
-    }
+    res.redirect('/');
   });
 });
+
+
 
 // Export routes for server.js to use.
 module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO burger (name) VALUES ('In-N-Out Burger');
-INSERT INTO burger (name) VALUES ('Burger King');
-INSERT INTO burger (name) VALUES ('Burger 3');
-INSERT INTO burger (name) VALUES ('Burger 4');
-INSERT INTO burger (name) VALUES ('Burger 5');
-INSERT INTO burger (name) VALUES ('Burger 6');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetable Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
